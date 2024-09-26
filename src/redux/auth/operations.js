b7b4ea0d60a8a39166f12b2640ba8be599a34de1
Alona Zabolotskykh <alenka.polishchuk@gmail.com>
2024-09-26 20:57:41 +0300

    fix auth

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -7,7 +7,7 @@ export const register = createAsyncThunk(
   "auth/register",
   async (credentials, thunkApi) => {
     try {
-      const { data } = await axios.post("/users/register", credentials);
+      const { data } = await axios.post("/users/signup", credentials);
       console.log(data);
       return data;
     } catch (error) {
